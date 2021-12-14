5a0470040c7b8ae5b8740662335aa25ac4007252
yingsz <lijun.ljun@bytedance.com>
2021-12-14 10:10:21 +0800

    sub gcdebt when push the table into the cache

--- lua-5.3.6/src/ltable.c
+++ lua-5.3.6/src/ltable.c
@@ -460,8 +460,7 @@ Table* luaH_newGuid(lua_State* L, unsigned short guid)
 		memset((void*)(tab->node), 0, sizenode(tab) * sizeof(Node));
 		tab->lastfree = gnode(tab, sizenode(tab));
 	}
-		
-	g->GCdebt = g->GCdebt + sizeof(Table) + tab->sizearray * sizeof(TValue) + sizenode(tab) * sizeof(Node);
+	g->GCdebt += sizeof(Table) + tab->sizearray * sizeof(TValue) + sizenode(tab) * sizeof(Node);
 	return tab;
 }
 
@@ -525,6 +524,7 @@ void luaH_free (lua_State *L, Table *t) {
 		Table* head = _alltables[t->guid];
 		gco->next = head ? obj2gco(head) : NULL;
 		_alltables[t->guid] = t;
+		g->GCdebt -= sizeof(Table) + t->sizearray * sizeof(TValue) + sizenode(tab) * sizeof(Node);
 		return;
 	}
 	luaH_freeimpl(L, t);
